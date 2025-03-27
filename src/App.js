adc8e11ee8f630fe1e51d56037289060a0c80e10
rujutasu <rujutasu@SHRAVANV-W10.cybage.com>
2025-03-27 10:50:08 +0530

    App component updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
-// import Nav from './Common/Nav.jsx';
 import ToDoItem from './components/ToDoItem.jsx';
 
 function App() {
   return (
     <div className="App">
       <div>
-        {/* <Nav></Nav> */}
         <ToDoItem></ToDoItem>
       </div>
     </div>
